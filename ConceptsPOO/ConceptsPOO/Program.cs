91ee9e54db7ad1a5afe11c63539b4092d1d26b7f
Manuel <davman_63@outlook.es>
2022-03-17 21:13:59 +0100

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}

609a9c61346c358a97b79ef1580e569223eb9de2
Manuel <davman_63@outlook.es>
2022-03-19 08:34:13 +0100

    Finish POO Concepts

---
+++ ConceptsPOO/ConceptsPOO/HourlyEmpleyee.cs
@@ -0,0 +1,19 @@
+﻿namespace ConceptsPOO
+{
+    public class HourlyEmpleyee : Employee
+    {
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HourValue;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tHours................: {$"{Hours:N2}",19}" +
+                $"\n\tHours value..........: {$"{HourValue:C2}",19}" +
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}",19}";
+        }
+    }
+}
